bd1b21beafa48829b4163cf2f6e1aa32183887fe
antoniotassone94 <antonio.tassone@studio.unibo.it>
2023-01-03 23:54:19 +0100

    importazione javascript e css all'interno delle viste (dalla cartella public)

---
+++ public/assets/js/app.js
@@ -0,0 +1,5 @@
+import 'bootstrap.js';
+
+function test(){
+    console.log("prova");
+}
--- resources/views/layout/biblios.blade.php
+++ resources/views/layout/biblios.blade.php
@@ -9,12 +9,32 @@
 <title>Biblios - @yield("title")</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="assets/js/app.js"></script>
+<link rel="stylesheet" type="text/css" href="assets/css/app.css">
 </head>
 <body>
-<a href="/">Homepage</a>&nbsp;<a href="/author">Archivio degli autori</a>&nbsp;<a href="/book">Archivio dei libri</a><br><br>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+    <a class="navbar-brand" href="/">Biblios</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="/author">Archivio degli autori</a>
+            <a class="nav-item nav-link" href="/book">Archivio dei libri</a>
+        </div>
+    </div>
+    <form class="form-inline my-2 my-lg-0">
+        <a class="btn btn-outline-success my-2 my-sm-0" href="#">Login</a>
+        <a class="btn btn-success my-2 my-sm-0" href="#">Registrati</a>
+    </form>
+</nav>
 <h1>@yield("title")</h1>
 <div id="content">
 @yield("content")
 </div>
+<script>
+test();
+</script>
 </body>
 </html>
